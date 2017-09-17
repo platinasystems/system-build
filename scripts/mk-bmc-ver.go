f7c8074b0a2401a49aa191a237fcf7e0534b3ae2
Mark Pleso <mpleso@platinasystems.com>
2017-09-17 08:21:23 -0700

    mk-bmc-ver.go: added file write of image info
    Signed-off-by: Mark Pleso <mpleso@platinasystems.com>

--- scripts/mk-bmc-ver.go
+++ scripts/mk-bmc-ver.go
@@ -2,43 +2,32 @@
 // Use of this source code is governed by the GPL-2 license described in the
 // LICENSE file.
 
-// mk-bmc-ver.go:  Make BMC Version Block
-
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
-	"os/user"
 	"strings"
 	"time"
 )
 
-type RELINFO struct {
-	Rel    [32]byte
-	Date   [32]byte
-	User   [32]byte
-	Unused [256 - 3*32]byte
-}
-type IMGINFO struct {
-	Name   [32]byte
-	Build  [32]byte
-	User   [16]byte
-	Size   [16]byte
-	GitTag [32]byte
-	Commit [64]byte
-	Chksum [64]byte
-}
 type IMAGE struct {
 	Name string
 	Dir  string
 	File string
 }
-
-var RelInfo RELINFO
-var ImgInfo [5]IMGINFO
-var VerBlock [256 * 1024]byte
+type IMGINFO struct {
+	Name   string
+	Build  string
+	User   string
+	Size   string
+	Tag    string
+	Commit string
+	Chksum string
+}
 
 var Images = [5]IMAGE{
 	{"ubo", "src/u-boot", "platina-mk1-bmc-ubo.bin"},
@@ -47,124 +36,103 @@ var Images = [5]IMAGE{
 	{"ker", "src/linux", "platina-mk1-bmc.vmlinuz"},
 	{"ini", "../go", "initrd.img.xz"},
 }
+var ImgInfo [5]IMGINFO
 
 func main() {
-	initQSPIdata()
-	getRelInfo() //TODO add arg for release
+	if len(os.Args) < 2 {
+		fmt.Println("Error no args")
+		os.Exit(1)
+	}
+	Release := getReleaseInfo(os.Args[1])
 	for i, _ := range Images {
 		getImageInfo(i, Images[i].Name, Images[i].Dir, Images[i].File)
 	}
-	writeVerFile()
+	writeVerFile(Release)
 }
 
-func initQSPIdata() {
-	for i, _ := range RelInfo.Rel {
-		RelInfo.Rel[i] = 0xff
-	}
-	for i, _ := range RelInfo.Date {
-		RelInfo.Date[i] = 0xff
-	}
-	for i, _ := range RelInfo.User {
-		RelInfo.User[i] = 0xff
-	}
-	for i, _ := range RelInfo.Unused {
-		RelInfo.Unused[i] = 0xff
-	}
-	for j, _ := range ImgInfo {
-		for i, _ := range ImgInfo[j].Name {
-			ImgInfo[j].Name[i] = 0xff
-		}
-		for i, _ := range ImgInfo[j].Build {
-			ImgInfo[j].Build[i] = 0xff
-		}
-		for i, _ := range ImgInfo[j].User {
-			ImgInfo[j].User[i] = 0xff
-		}
-		for i, _ := range ImgInfo[j].Size {
-			ImgInfo[j].Size[i] = 0xff
-		}
-		for i, _ := range ImgInfo[j].GitTag {
-			ImgInfo[j].GitTag[i] = 0xff
-		}
-		for i, _ := range ImgInfo[j].Commit {
-			ImgInfo[j].Commit[i] = 0xff
-		}
-		for i, _ := range ImgInfo[j].Chksum {
-			ImgInfo[j].Chksum[i] = 0xff
-		}
-	}
-	for i, _ := range VerBlock {
-		VerBlock[i] = 0xff
-	}
-}
-
-func getRelInfo() {
-	//TODO ADD NAME //DEV
+func getReleaseInfo(k string) string {
 	t := time.Now()
-	fmt.Println("NOW: ", t.Format("2006-01-02 15:04:05"))
-	user, _ := user.Current()
-	fmt.Println("BY: ", user.Username)
+	kk := ""
+	switch k {
+	case "dev":
+		kk = "dev"
+	case "rel":
+		kk = t.Format("20060102")
+	default:
+		fmt.Println("Error dev or rel not found")
+		os.Exit(1)
+	}
+	return kk
 }
 
 func getImageInfo(x int, nm string, di string, im string) {
 	u, err := exec.Command("ls", "-l", im).Output()
 	if err != nil {
-		fmt.Println("ERR1A")
-		return
+		fmt.Println("Error ls")
+		os.Exit(1)
 	}
 	uu := strings.Split(string(u), " ")
 	t := time.Now()
 	yr := t.Format("2006")
-	fmt.Println("platina-mk-bmc-" + nm + ".bin")              //name
-	fmt.Println(uu[2])                                        //user
-	fmt.Println(uu[5] + " " + uu[6] + " " + yr + " " + uu[7]) //date
-	fmt.Println(uu[4])                                        //size
+	ImgInfo[x].Name = "platina-mk-bmc-" + nm + ".bin"
+	ImgInfo[x].Build = uu[5] + " " + uu[6] + " " + yr + " " + uu[7]
+	ImgInfo[x].User = uu[2]
+	ImgInfo[x].Size = uu[4]
 
 	{
 		od, err := os.Getwd()
 		if err != nil {
-			fmt.Println("ERR0") //TODO et al
-			return
+			fmt.Println("Error getting working directory")
+			os.Exit(1)
 		}
 		err = os.Chdir(di)
 		if err != nil {
-			fmt.Println(err)
-			return
+			fmt.Println("Error changing directory")
+			os.Exit(1)
 		}
 		defer os.Chdir(od)
-
-		u, err = exec.Command("git", "tag").Output()
+		u, err = exec.Command("git", "describe", "--abbrev=0").Output()
 		if err != nil {
-			fmt.Println("ERR2")
-			return
+			fmt.Println("Error running git describe")
+			os.Exit(1)
 		}
 		uu = strings.Split(string(u), "\n")
-		//TODO sort tags, find latest
-		fmt.Println(uu[len(uu)-2]) //git tag
-
+		ImgInfo[x].Tag = (uu[0])
 		u, err = exec.Command("git", "log", "-1").Output()
 		if err != nil {
-			fmt.Println("ERR2")
-			return
+			fmt.Println("Error running git log")
+			os.Exit(1)
 		}
 		uu = strings.Split(string(u), "\n")
 		uuu := strings.Split(string(uu[0]), " ")
-		fmt.Println(uuu[1]) //git commit
-
+		ImgInfo[x].Commit = (uuu[1])
 		err = os.Chdir(od)
+		if err != nil {
+			fmt.Println("Error restoring working directory")
+			os.Exit(1)
+		}
 	}
-
 	u, err = exec.Command("sha1sum", im).Output()
 	if err != nil {
-		fmt.Println("ERR1")
-		return
+		fmt.Println("Error running sha1sum")
+		os.Exit(1)
 	}
 	uu = strings.Split(string(u), "\n")
 	uuu := strings.Split(string(uu[0]), " ")
-	fmt.Println(uuu[0]) //checksum
-
-	//TODO add data to block instead of printing
+	ImgInfo[x].Chksum = (uuu[0])
 }
 
-func writeVerFile() { //TODO
+func writeVerFile(Release string) {
+	VerBlock := make([]byte, 256*1024)
+	for i, _ := range VerBlock {
+		VerBlock[i] = 0xff
+	}
+	copy(VerBlock[0x00:], Release)
+	jsonInfo, _ := json.Marshal(ImgInfo)
+	copy(VerBlock[0x100:], jsonInfo)
+	err := ioutil.WriteFile("platina-mk1-bmc-ver.bin", VerBlock, 0644)
+	if err != nil {
+		fmt.Println("Error writing version file")
+		os.Exit(1)
+	}
 }
